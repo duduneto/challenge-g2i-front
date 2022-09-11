7d15c7e38d49f4572536971ba940edf0d4b19a10
Dudu Neto <dudumatosneto@gmail.com>
2022-09-11 20:07:12 -0300

    docs: create question step

---
+++ src/screens/Quiz/QuestionStep/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const QuestionStep: React.FC = () => {
+  return (
+    <Container>
+        <h4>0 of 1</h4>
+    </Container>
+  );
+}
+
+export default QuestionStep;
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Question from './Question';
+import QuestionStep from './QuestionStep';
 
 import { Container } from './styles';
 
 const Quiz: React.FC = () => {
   return (
     <Container>
+      <QuestionStep />
       <Question />
     </Container>
   );
