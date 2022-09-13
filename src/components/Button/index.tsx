7382f87e207fd3baa66f6c2c847ed73056db1803
Dudu Neto <dudumatosneto@gmail.com>
2022-09-12 23:24:48 -0300

    add styles to btn

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,6 +4,7 @@ import { ButtonComp } from './styles';
 
 interface IButton extends React.HTMLAttributes<HTMLButtonElement> {
     width?: string;
+    btnStyle?: 'success' | 'danger' | 'normal';
 }
 
 const Button: React.FC<IButton> = (props) => {
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,16 +2,41 @@ import styled from 'styled-components';
 
 type ButtonT = {
   width?: string;
+  btnStyle?: 'success' | 'danger' | 'normal';
+};
+
+const buttonStyle = {
+  success: {
+    color: 'white',
+    backgroundColor: '#00923f'
+  },
+  danger: {
+    color: 'white',
+    backgroundColor: '#ff0000'
+  },
+  normal: {
+    color: 'black',
+    backgroundColor: '#FFFFFF'
+  },
 }
 
 export const ButtonComp = styled.button<ButtonT>`
   background-color: white;
   min-width: 100px;
-  width: ${({width}) => width || 'auto'};
+  width: ${({ width }) => width || 'auto'};
   outline: none;
   border: 1px solid #797979;
   padding: 0.5rem 1rem;
   cursor: pointer;
+
+  background-color: ${({ btnStyle }) => btnStyle ?
+    buttonStyle[btnStyle].backgroundColor :
+    buttonStyle.normal.backgroundColor
+  };
+  color: ${({ btnStyle }) => btnStyle ?
+    buttonStyle[btnStyle].color :
+    buttonStyle.normal.color
+  };
 
   &:active {
     background-color: #797979;
